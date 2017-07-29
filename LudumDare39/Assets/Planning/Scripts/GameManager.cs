c0e9b95f00174a6f4440be3b44fae72eb6e1fea3
Riccardo Rizzo <rizzoricky@gmail.com>
2017-07-29 20:32:01 +0200

    game manager

--- LudumDare39/Assets/Planning/Scripts/GameManager.cs
+++ LudumDare39/Assets/Planning/Scripts/GameManager.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct Scene
+{
+    [SerializeField]
+    private string m_name;
+}
+
 public class GameManager : MonoBehaviour
 {
     Singleton<GameManager> m_gameManager;
 
-    public GameObject m_sceneLoader = null;
+    [SerializeField]
+    private GameObject m_sceneLoader = null;
+
+    [SerializeField]
+    private List<Scene> m_scenesNames = null;
 
     public float m_currentPsychophysicsValue = 0.0f;
 
@@ -14,11 +25,12 @@ public class GameManager : MonoBehaviour
 
     public float m_currentSocialValue = 0.0f;
 
-    private m_sceneLoaderManager = null;
+    private SceneLoaderManager m_sceneLoaderManager = null;
 
     void Start()
     {
         m_sceneLoaderManager = m_sceneLoader.GetComponent<SceneLoaderManager>();
+        m_sceneLoaderManager.LoadScenes();
     }
 
     void Update()
