3f1caaeca8cafe1009236bc173477a11102bf7d5
gianluca goberti <gianluca.goberti@gmail.com>
2017-07-29 20:29:55 +0200

    First Commit -Add script for letter fall at a velocity -Add a game manager(TypingManager) -Add script for create the ui(maybe delete later because the manager create all now) -Add script for the trigger of a letter -Add some canvas

---
+++ LudumDare39/Assets/Scripts/LetterFall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterFall : MonoBehaviour {
+    [SerializeField]
+    private float velocity = 1.0f;
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, (velocity * -1.0f), 0);
+	}
+
+}
---
+++ LudumDare39/Assets/Scripts/TypingManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TypingManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject point;
+    [SerializeField]
+    private int numLet = 5;
+
+    [SerializeField]
+    private Canvas canvas;
+
+    // Use this for initialization
+    void Start()
+    {
+        Camera camera = FindObjectOfType<Camera>();
+        Canvas newCanvas = Instantiate(canvas);
+        newCanvas.worldCamera = camera;
+
+       
+
+        float left = camera.orthographicSize * camera.aspect * -1;
+        float space = (camera.orthographicSize * camera.aspect * 2) / (numLet + 1);
+        for (int i = 1; i <= numLet; i++)
+        {
+            GameObject bru = Instantiate(point, new Vector3(left + (space * i), (camera.orthographicSize * 0.9f) * -1, 0), Quaternion.identity, newCanvas.transform);//0.9f= 90% of camera hight
+
+            bru.name = point.name + i;
+            LetterTrigger letterListener = bru.AddComponent<LetterTrigger>();
+            letterListener.onLetterHit += OnLetterHit;
+        }
+
+    }
+
+    public void OnLetterHit()
+    {
+        Debug.Log("bruuuuu");
+    }
+}
