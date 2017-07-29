39f7380540fd92550b2ee2fa13c267bcb57bf370
Alessio Bendoni <alessio.bendoni91@gmail.com>
2017-07-29 23:40:10 +0200

    - Compleated drag and drop list - added a test scene

---
+++ LudumDare39/Assets/Scripts/MinigameInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinigameInfo : MonoBehaviour {
+
+	[SerializeField]
+	private Text m_NameText;
+	[SerializeField]
+	private Text m_PsyphyText;
+    [SerializeField]
+    private Text m_MoneyText;
+    [SerializeField]
+    private Text m_SocialText;
+
+    private string m_Name;
+
+	public void Setup (string i_Name, float i_PsychoPhysicsValue, float i_MoneyValue, float i_SocialValue )
+	{
+		m_Name = i_Name;
+		m_NameText.text = i_Name;
+		m_PsyphyText.text = "" + i_PsychoPhysicsValue;
+		m_MoneyText.text = "" + i_MoneyValue;
+		m_SocialText.text = "" + i_SocialValue;
+	}
+}
---
+++ LudumDare39/Assets/Scripts/Slot.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IDropHandler
+{
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            DragHandler.itemDragged.transform.SetParent(transform);
+            DragHandler.itemDragged.transform.position = transform.position;
+        }
+    }
+}
