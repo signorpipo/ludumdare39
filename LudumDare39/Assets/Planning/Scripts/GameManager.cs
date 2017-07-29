40aec80cbab823fcbea441ea0a45ac8edf17caba
Riccardo Rizzo <rizzoricky@gmail.com>
2017-07-29 20:31:44 +0200

    Singleton and GameManager
    Singleton and GameManager prototype

---
+++ LudumDare39/Assets/Planning/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    Singleton<GameManager> m_gameManager;
+
+    public GameObject m_sceneLoader = null;
+
+    public float m_currentPsychophysicsValue = 0.0f;
+
+    public float m_currentMoneyValue = 0.0f;
+
+    public float m_currentSocialValue = 0.0f;
+
+    private m_sceneLoaderManager = null;
+
+    void Start()
+    {
+        m_sceneLoaderManager = m_sceneLoader.GetComponent<SceneLoaderManager>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void LoadNextScene()
+    {
+
+    }
+}
+
