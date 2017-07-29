a2fdfc05bb058087c17f9655f3967b5aa1044f6f
Riccardo Reffato <riccardo.reffato@gmail.com>
2017-07-29 17:43:50 +0200

    Primo commit livello Dodge

---
+++ LudumDare39/Assets/Scripts/dodge_minigame/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float m_HorizontalSpeed = 10.0f;
+
+    private float m_CameraWidthSize = 0.0f;
+
+ 
+    private float m_MyHalfWidthSize = 0.0f;
+
+
+    private void Awake()
+    {
+        m_CameraWidthSize = Camera.main.orthographicSize * Camera.main.aspect;
+        m_MyHalfWidthSize = (GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2)*transform.localScale.x;
+    }
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 currentPosition = transform.position;
+
+        Vector3 newPosition = new Vector3( currentPosition.x + horizontalInput*m_HorizontalSpeed*Time.deltaTime , currentPosition.y, currentPosition.z);
+
+        if( (Mathf.Abs(newPosition.x)+m_MyHalfWidthSize) <= m_CameraWidthSize )
+        {
+            transform.position = newPosition;
+        }
+
+    }
+}
---
+++ LudumDare39/Assets/Scripts/dodge_minigame/SpawnManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    [SerializeField]
+    private int m_EnemyItemPerSeconds = 1;
+
+
+    private float m_CameraWidthSize = 0.0f;
+
+    [SerializeField]
+    private GameObject m_EnemyItemPrefab;
+
+    private float m_ElapsedTime = 0.0f;
+
+    private void Awake()
+    {
+        m_CameraWidthSize = Camera.main.orthographicSize * Camera.main.aspect;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_ElapsedTime += Time.deltaTime;
+
+        if(m_ElapsedTime >= 1.0f)
+        {
+            for(int i=0; i < m_EnemyItemPerSeconds; i++)
+            {
+                Instantiate(m_EnemyItemPrefab, new Vector3(Random.Range(-m_CameraWidthSize, m_CameraWidthSize), Camera.main.orthographicSize, 0), Quaternion.identity);
+            }
+            m_ElapsedTime = 0.0f;
+        }
+
+    }
+}
