f998ef8e5c0464cad330c53d3d6dc3ccebb3e5a2
Riccardo Rizzo <rizzoricky@gmail.com>
2017-07-29 18:47:31 +0200

    Data and infos to be used in planning phase
    - Created MenuManager for communication between data and UI
    - Created MinigameInterface for storing data infos
    - Created StatsCalculator for calculating graphs output data

---
+++ LudumDare39/Assets/Planning/Scripts/MenuManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<MinigameInterface> m_minigames;
+
+    [SerializeField]
+    private int m_numberOfSelectedGames = 3;
+
+    private List<MinigameInterface> m_seletedMinigames;
+
+    // riferimento a panel pool minigames
+
+    // riferimento a panel selected minigames
+
+	// Use this for initialization
+	void Start ()
+    {
+        // per ogni oggetto in lista dei minagames
+            // crea un "bottone" nel panel pool
+            // imposta informazioni su panel pool minigames
+	}
+
+    public void StartGame()
+    {
+        // Per ogni oggetto in lista di selezione
+            // salva informazioni sul minigame
+            // verifica che siano consistenti (grandezza lista == 3)
+            // invoca metodo da SceneLoader del tipo LoadScenes(scene1, scene2, scene3);
+    }
+
+    void Update()
+    {
+        // se la lista della UI è completa
+        for (int index = 0; index < m_numberOfSelectedGames; ++index)
+        {
+            // se esiste qualcosa a quella posizione sulla lista dell'interfaccia
+            m_seletedMinigames.Add(new MinigameInterface()); // add minigame interface at position index
+        }
+    }
+
+    public List<MinigameInterface> GetSelectedMinigamesList()
+    {
+        return m_seletedMinigames;
+    }
+}
---
+++ LudumDare39/Assets/Planning/Scripts/StatsCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsCalculator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_menuManagerPrefab = null;
+
+    private MenuManager m_menuManager = null;
+
+    private float m_finalPsychophysicsOutputValue = 0.0f;
+
+    private float m_finalMoneyOutputValue = 0.0f;
+
+    private float m_finalSocialOutputValue = 0.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_menuManager = m_menuManagerPrefab.GetComponent<MenuManager>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        List<MinigameInterface> m_seletedMinigames = m_menuManager.GetSelectedMinigamesList();
+        int numberOfMinigames = m_seletedMinigames.Count;
+
+        for (int index = 0; index < numberOfMinigames; ++index)
+        {
+            m_finalPsychophysicsOutputValue += m_seletedMinigames[index].GetPsychophysicsOutputValue();
+            m_finalMoneyOutputValue += m_seletedMinigames[index].GetMoneyOutputValue();
+            m_finalSocialOutputValue += m_seletedMinigames[index].GetSocialOutputvalue();
+        }
+
+        m_finalPsychophysicsOutputValue /= numberOfMinigames;
+        m_finalMoneyOutputValue /= numberOfMinigames;
+        m_finalSocialOutputValue /= numberOfMinigames;
+    }
+}
