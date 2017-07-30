aab0aa54af6af44cd45f2105e5dbdae8121255a7
Diego Zamprogno <nurbed@gmail.com>
2017-07-30 19:58:34 +0200

    Background Manager

---
+++ LudumDare39/Assets/Colorize.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colorize : MonoBehaviour
+{
+    public List<SpriteRenderer> MainColor;
+    public List<SpriteRenderer> SecondaryColor;
+    public List<SpriteRenderer> ThirdColor;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
