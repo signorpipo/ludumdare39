8cea656c099a2310203df723907becc0b7a764f1
gianluca <gianluca.goberti@gmail.com>
2017-07-30 12:31:05 +0200

    -Add folder Typing Minigame in Scripts and Prefabs -Moved file in the new folder -Minigame acctually works

---
+++ LudumDare39/Assets/Scripts/Typing Minigame/LetterTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void OnLetterHit(GameObject letterFall);
+
+public class LetterTrigger : MonoBehaviour
+{
+    private bool inside= false;
+    private GameObject objectTriggered;
+    private KeyCode myKey;
+    public KeyCode MyKey
+    {
+        get { return myKey; }
+        set { myKey = value; }
+    }
+
+    public OnLetterHit onLetterHit = null;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        inside = true;
+        objectTriggered = collision.gameObject;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        inside = false;
+        objectTriggered = null;
+    }
+
+    private void FixedUpdate()
+    {
+        if(inside && (Input.GetKeyDown(myKey)) && onLetterHit != null)
+        {
+            onLetterHit(objectTriggered);
+        }
+    }
+}
