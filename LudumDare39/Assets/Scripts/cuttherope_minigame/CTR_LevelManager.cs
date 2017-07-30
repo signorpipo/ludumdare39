01676b0753f7742219ce76e44029e19c7b965450
Riccardo Reffato <riccardo.reffato@gmail.com>
2017-07-30 17:20:43 +0200

    upload cut the rope

---
+++ LudumDare39/Assets/Scripts/cuttherope_minigame/CTR_LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CTR_LevelManager : MonoBehaviour {
+    [SerializeField]
+    public RobeDraw m_Rope;
+
+    [SerializeField]
+    public GameObject m_Ball;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButton(0))
+        {
+            m_Ball.GetComponent<FixedJoint2D>().enabled = false;
+            m_Rope.RemoveLastJount();
+
+        }
+	}
+}
