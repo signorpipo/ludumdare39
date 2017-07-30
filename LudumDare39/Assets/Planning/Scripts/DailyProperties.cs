90414b0f5870aa14aac0ab4e36059ae1e14712a7
Riccardo Rizzo <rizzoricky@gmail.com>
2017-07-31 00:22:15 +0200

    Daily bonuses GameManager update

---
+++ LudumDare39/Assets/Planning/Scripts/DailyProperties.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DailyProperties : MonoBehaviour
+{
+    public float m_psychophysicsBonus;
+    public float m_moneyBonus;
+    public float m_socialBonus;
+}
