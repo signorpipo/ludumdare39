91fe4a451af99d64fab5bc816cf5106fb9b827dc
Diego Zamprogno <nurbed@gmail.com>
2017-07-30 10:56:19 +0200

    Test item

--- LudumDare39/Assets/Scripts/MinigameManagers/ConnectGameManager.cs
+++ LudumDare39/Assets/Scripts/MinigameManagers/ConnectGameManager.cs
@@ -8,10 +8,23 @@ public class ConnectGameManager : MinigameManager
     public RectTransform LeftPanel;
     public RectTransform Rightpanel;
 
+    public List<Sprite> GameItems;
+
+    public int NumGameItems = 0;
+
+    public MonoBehaviour ItemPrefab;
+
     // Use this for initialization
     public override void Start () {
+        float deltaHeight = LeftPanel.GetComponent<RectTransform>().sizeDelta.y;
+        for (int i =0; i<NumGameItems; ++i)
+        {
+            MonoBehaviour itm = Instantiate(ItemPrefab, LeftPanel);
+            itm.GetComponent<Image>().sprite = GameItems[(int)(Random.value * GameItems.Count) % GameItems.Count];
+            //itm.GetComponent<RectTransform>().SetPositionAndRotation(Vector3.up * deltaHeight, Quaternion.identity);
+        }
+
         base.Start();
-		
 	}
 
     // Update is called once per frame
