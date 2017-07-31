02db2c1329f07ec467c197857d0e25dafe16fc15
Toccio <eliaducceschi@hotmail.it>
2017-07-31 21:19:14 +0200

    Sicuramente non pepe

---
+++ LudumDare39/Assets/Scripts/PepeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PepeScript : MonoBehaviour {
+
+    [SerializeField]
+    private Animator m_Animator;
+
+    private bool m_CanPlayAnimation = true;
+
+    private void Update()
+    {
+        if (m_CanPlayAnimation)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                m_CanPlayAnimation = false;
+                m_Animator.Play("PepeTheKing",0);
+            }
+        }
+    }
+
+    private void AnimationEnd()
+    {
+        m_CanPlayAnimation = true;
+    }
+}
